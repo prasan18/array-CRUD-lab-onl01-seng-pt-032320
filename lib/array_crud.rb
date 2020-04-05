63d566c688dab97cd770d38a4bea5f4eecda806c
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-05 17:12:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -27,7 +27,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.shift("wow")
+  array.shift(wow)
 end
 
 def retrieve_element_from_index(array, index_number)
