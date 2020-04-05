729369bcce973ea38b4bc07fa1d5addf0fb092d7
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-05 16:56:09 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -9,6 +9,7 @@ end
 def add_element_to_end_of_array(array, element)
  array = ["wow", "I", "am", "really", "learning"]
     element = "arrays!"
+    array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
