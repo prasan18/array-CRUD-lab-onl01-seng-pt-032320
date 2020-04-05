a54475aef6277255fa587e3834e0e38891790c05
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-05 16:50:54 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -6,9 +6,8 @@ def create_an_array
   famous_cats = ["bell","dell","spell","fell"]
 end
 
-def add_element_to_end_of_array(array,element)
+def add_element_to_end_of_array(array, element)
   array << element
-  binding.pry
 end
 
 def add_element_to_start_of_array(array, element)
