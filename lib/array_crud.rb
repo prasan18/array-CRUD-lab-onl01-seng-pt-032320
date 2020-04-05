722b8d35b81ffb9cad1c7bb2c48b02aacf04dacf
ANTONY JUDE PRASAN POOBALARAYAN <prasan29@yahoo.com>
2020-04-05 16:47:37 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -1,13 +1,13 @@
-def create_an_empty_array
-  
+def create_an_empty_array 
+  []
 end
 
 def create_an_array
-  
+  famous_cats = ["bell","dell","spell","fell"]
 end
 
-def add_element_to_end_of_array(array, element)
-  
+def add_element_to_end_of_array(famous_cat,hell)
+  famous_cats.push("hell")
 end
 
 def add_element_to_start_of_array(array, element)
